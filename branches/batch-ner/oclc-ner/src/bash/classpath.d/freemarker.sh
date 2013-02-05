353f382ed126e74a3ba0dc1d541ffb6ba82ea6f3
decasm <decasm@27c5f2fa-5ce0-7e9b-5889-3d756d74b938>
2013-02-05 16:59:56 +0000

    Added classpath.d CP configuration; add FreeMarker to ner.server;

--- branches/batch-ner/oclc-ner/src/bash/classpath.d/freemarker.sh
+++ branches/batch-ner/oclc-ner/src/bash/classpath.d/freemarker.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Begin tagger
+# Begin freemarker.sh
 ##############
 #$# creator : Devon Smith
 #%# email: smithde@oclc.org
-#$# created : 2010-06-22
-#$# title : tagger
-#$# description : Command line tagger demo 
+#$# created : 2010-06-18
+#$# title : freemarker.sh
+#$# description : Add FreeMarker jars to the classpath
 ########## Define a usage function 
 function usage {
-    echo "Usage: $this [ format ]"
-    echo -e "\t default format = text/x-ner-markup"
-    echo -e "\t valid format options = text/x-ner-entities, text/html, application/xml"
+    echo "Usage: $this <library directory>"
     if [ "$1" = "exit" ]; then exit; fi
 }
 ########## Set to "false" to turn verbose off
@@ -18,7 +16,6 @@ function echo_stdout { echo "$@"; }
 function echo_stderr { echo "$@" >& 2; }
 # Set verbose to 'echo_stderr' or 'echo_stdout' to see output trace info
 verbose=false
-verbose=echo_stdout
 $verbose Verbose output on ${verbose##*_}
 
 ########## Setup some common paths
@@ -38,39 +35,20 @@ $verbose Time: $today $now
 ##########
 if [ "$1" = "-h" ]; then usage 'exit' ; fi
 ##########
-JAVA=$(which java)
 
-format="text/x-ner-markup"
-if [ "$1" != "" ]
-then
-	format="$1"
-fi
-
-moduledir=$(dirname $(dirname $here))
-$verbose Module Dir: $moduledir
-
-libdir=$(dirname $moduledir)/lib
+libdir=$1
 $verbose Library Dir: $libdir
+if [ ! -d $libdir ]; then echo "Arg #1 (\"$libdir\") is not a directory - FreeMarker JARs not found'"; exit; fi
 
-for script in $(ls classpath.d/*)
-do
-	if [ -x $script ]
-	then
-		cp=$($script $libdir)
-		if [ "" == "$CP" ]; then CP=$cp; else CP=$CP:$cp; fi
-	fi
-done
+freemarker_libdir=$libdir/freemarker-2.3.19/lib
+$verbose FreeMarker Library Dir: $freemarker_libdir
+freemarker_jars=(freemarker.jar)
+
+for jar in ${freemarker_jars[*]}; do CP=${CP:+$CP:}$freemarker_libdir/$jar; done
 $verbose Classpath: $CP
 
-CLASS=org.oclc.gateman.CmdLineTagger
-# Usage: org.oclc.gateman.CmdLineTagger -d input-dir output-dir [ format ]
-# Usage: org.oclc.gateman.CmdLineTagger -f batch-file output-file tagger-properties
-
-input_dir=input
-output_dir=output
-
-$JAVA -Xmx2000m -cp $CP $CLASS -d $input_dir $output_dir $format
+echo $CP
 
 ############
-# End tagger
+# End freemarker.sh
 # vim:ts=4:indentexpr=
