254790dab5120b20483d4bcdf9a58a4a6daefc95
decasm <decasm@27c5f2fa-5ce0-7e9b-5889-3d756d74b938>
2013-02-05 17:15:50 +0000

    Moved formatting logic to Formatter class

--- oclc-ner/src/java/org/oclc/gateman/ner/Tagger.java
+++ oclc-ner/src/java/org/oclc/gateman/ner/Tagger.java
@@ -4,13 +4,22 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.StringWriter;
 
 import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Vector;
 import java.util.StringTokenizer;
 import java.util.Hashtable;
+import java.util.logging.Logger;
 
 import java.net.URL;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 import LBJ2.parse.LinkedVector;
 import LBJ2.classify.Classifier;
@@ -33,6 +42,8 @@ import LbjTagger.ShapeClassifierManager;
 public class Tagger extends NerBase {
 
 	private BufferedReader reader = null;
+
+	protected static Logger logger = Logger.getLogger("org.oclc.gateman.GatemanApplication");;
 
 	private StringBuilder prepBuf;
 	private boolean prepared = false;
@@ -58,6 +69,8 @@ public class Tagger extends NerBase {
 		return tagger;
 	}
 
+
+	public Vector<LinkedVector> getData() { return data; }
 
 	/* Set the input source
 	 */
@@ -94,241 +107,5 @@ public class Tagger extends NerBase {
 		data = BracketFileManager.parseText(prepBuf.toString());
 		NETester.annotateBothLevels(data,tagger1,tagger2);
 	}
-
-
-	/* Format the results as HTML
-	 * Find a template language?
-	 */
-	public String formatResultsHTML(String link) {
-		StringBuilder result = new StringBuilder("<html><head>")
-			.append("<style type='text/css'>th{text-align:left;}span{font-weight:bold;}")
-			.append(".per{color:red;}.loc{color:blue;}.org{color:green;}.misc{color:purple;}")
-			.append("table#key{float:left;font-size:small;margin-right:5pt;margin-bottom:5pt;border-right:solid thin black;border-bottom:solid thin black;")
-			.append("</style></head><body>");
-		if ( null != link && ! link.equals("") ) {
-			result.append("<p><a href=\"").append(link).append("\">").append(link).append("</a></p>");
-		}
-		result.append("<hr/><div id='content'>")
-				.append("<table id='key'>")
-				.append("<tr><th class='per'>Person</th></tr>")
-				.append("<tr><th class='loc'>Location</th></tr>")
-				.append("<tr><th class='org'>Organization</th></tr>")
-				.append("<tr><th class='misc'>Misc</th></tr>")
-				.append("</table>");
-
-		for (int i = 0; i < data.size(); i++) {
-			LinkedVector vector = data.elementAt(i);
-			StringBuilder res = new StringBuilder();
-			boolean open = false;
-			String[] predictions = new String[vector.size()];
-			String[] words = new String[vector.size()];
-			for (int j = 0; j < vector.size(); j++) {
-				predictions[j] = bilou2bio(((NEWord)vector.get(j)).neTypeLevel2);
-				words[j] = ((NEWord)vector.get(j)).form;
-			}
-			String spanClass = "error"; // If not overridden, but used, it's an error
-			for (int j = 0; j < vector.size(); j++) {
-				if (predictions[j].startsWith("B-")
-					|| (j > 0 && predictions[j].startsWith("I-")
-					&& (! predictions[j-1].endsWith(predictions[j].substring(2))))){
-						res.append("<span class=\"" + predictions[j].substring(2).toLowerCase() + "\">");
-						open = true;
-				}
-				res.append(words[j]);
-				if (open) {
-					boolean close = false;
-					if (j == vector.size()-1) {
-						close = true;
-					}
-					else if (predictions[j+1].startsWith("B-")) {
-						close=true;
-					}
-					else if (predictions[j+1].equals("O")) {
-						close=true;
-					}
-					else if (predictions[j+1].indexOf('-')>-1&&(!predictions[j].endsWith(predictions[j+1].substring(2)))) {
-						close=true;
-					}
-					if (close) {
-						res.append("</span> ");
-						open = false;
-					}
-					else {
-						res.append(" ");
-					}
-				}
-				else {
-					res.append(" ");
-				}
-			}
-			result.append(res.toString());
-		}
-		return result.append("</div><hr/></body></html>").toString();
-	}
- 
-	/* Format the results as XML
-	 * Find a template language?
-	 */
-	public String formatResultsXML() {
-		StringBuilder result = new StringBuilder("<ner:content xmlns:ner=\"uri:ns:ner\">");
-
-		for (int i = 0; i < data.size(); i++) {
-			LinkedVector vector = data.elementAt(i);
-			StringBuilder res = new StringBuilder();
-			boolean open = false;
-			String[] predictions = new String[vector.size()];
-			String[] words = new String[vector.size()];
-			for (int j = 0; j < vector.size(); j++) {
-				predictions[j] = bilou2bio(((NEWord)vector.get(j)).neTypeLevel2);
-				words[j] = ((NEWord)vector.get(j)).form;
-			}
-			String tag = "ner:ERROR"; // If not overridden, but used, it's an error
-			for (int j = 0; j < vector.size(); j++) {
-				if (predictions[j].startsWith("B-")
-					|| (j > 0 && predictions[j].startsWith("I-")
-					&& (! predictions[j-1].endsWith(predictions[j].substring(2))))){
-						tag = "ner:" + predictions[j].substring(2).toLowerCase(); 
-						res.append("<" + tag + ">");
-						open = true;
-						
-				}
-				res.append(words[j]);
-				if (open) {
-					boolean close = false;
-					if (j == vector.size()-1) {
-						close = true;
-					}
-					else if (predictions[j+1].startsWith("B-")) {
-						close=true;
-					}
-					else if (predictions[j+1].equals("O")) {
-						close=true;
-					}
-					else if (predictions[j+1].indexOf('-')>-1&&(!predictions[j].endsWith(predictions[j+1].substring(2)))) {
-						close=true;
-					}
-					if (close) {
-						res.append("</" + tag + "> ");
-						open = false;
-					}
-					else {
-						res.append(" ");
-					}
-				}
-				else {
-					res.append(" ");
-				}
-			}
-			result.append(res.toString());
-		}
-		return result.append("</ner:content>").toString();
-	}
-    
-
-	/* Format the results as text/x-ner-markup
-	 */
-	public String formatResults() {
-		StringBuilder result = new StringBuilder();
-
-		for (int i = 0; i < data.size(); i++) {
-			LinkedVector vector = data.elementAt(i);
-			StringBuilder res = new StringBuilder();
-			boolean open = false;
-			String[] predictions = new String[vector.size()];
-			String[] words = new String[vector.size()];
-			for (int j = 0; j < vector.size(); j++) {
-				predictions[j] = bilou2bio(((NEWord)vector.get(j)).neTypeLevel2);
-				words[j] = ((NEWord)vector.get(j)).form;
-			}
-			for (int j = 0; j < vector.size(); j++) {
-				if (predictions[j].startsWith("B-")
-					|| (j > 0 && predictions[j].startsWith("I-") && (! predictions[j-1].endsWith(predictions[j].substring(2))))){
-
-					res.append("[" + predictions[j].substring(2) + " ");
-					open = true;
-				}
-				res.append(words[j]+ " ");
-				if (open) {
-					boolean close = false;
-					if (j == vector.size()-1) {
-						close = true;
-					}
-					else {
-						if (predictions[j+1].startsWith("B-")) {
-							close=true;
-						}
-						if (predictions[j+1].equals("O")) {
-							close=true;
-						}
-						if (predictions[j+1].indexOf('-')>-1&&(!predictions[j].endsWith(predictions[j+1].substring(2)))) {
-							close=true;
-						}
-					}
-					if (close) {
-						res.append(" ] ");
-						open = false;
-					}
-				}
-			}
-			result.append(res.toString()).append("\n");
-		}
-		return result.toString();
-	}
-
-	/*
- 	 * "Format" the results to present the raw data
-	 */
-	public String formatResultsENT() {
-		StringBuilder result = new StringBuilder();
-		for (int i = 0; i < data.size(); i++) {
-			LinkedVector vector = data.elementAt(i);
-			for (int j = 0; j < vector.size(); j++) {
-				String prediction = ((NEWord)vector.get(j)).neTypeLevel2;
-				if ( prediction.equals("O") ) {
-					continue;
-				}
-				prediction = bilou2bio(prediction);
-
-				if (prediction.startsWith("B-")) {
-					result.append( prediction.substring(2) ).append("\t");
-				}
-				result.append( ((NEWord)vector.get(j)).form );
-
-				if ( j < vector.size() ) {
-					NEWord la = (NEWord)vector.get(j+1);
-					if ( null != la ) {
-						String lookahead = la.neTypeLevel2;
-						lookahead = bilou2bio(lookahead);
-	
-						if ( null != lookahead && lookahead.startsWith("I-") ) {
-							result.append(" ");
-						}
-						else {
-							result.append("\n");
-						}
-					}
-					else {
-						result.append("\n");
-					}
-				}
-			}
-		}
-		return result.toString();
-	}
-    
-	/* ? Not sure, copied from UIUC code, used by formatResults
-	 */
-	private String bilou2bio(String prediction){
-		if (Parameters.taggingScheme.equalsIgnoreCase(Parameters.BILOU)) {
-			if (prediction.startsWith("U-")) {
-				prediction="B-"+prediction.substring(2);
-			}
-			if (prediction.startsWith("L-")) {
-				prediction="I-"+prediction.substring(2);
-			}
-		}
-		return prediction;
-	}
- 
 }
 
